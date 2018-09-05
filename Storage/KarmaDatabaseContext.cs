d5a5a84a7433c1bf3e0a4a7bea20f5b7cce53fdc
gderaco <giuseppe.deraco@gmail.com>
2018-09-05 18:01:24 +0200

    Changed port and exluded db journal

--- Storage/KarmaDatabaseContext.cs
+++ Storage/KarmaDatabaseContext.cs
@@ -22,6 +22,8 @@ public class KarmaDatabaseContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder builder)
     {
+        //insert into Channels("Name") values ("#abc");
+        //insert into Channels("Name") values ("#cba")
         base.OnConfiguring(builder);
     }
 }
